eef51c19e0f6c6314aa3829b2352e8d6ed4476ac
Chris Archibald <carchi8py@gmail.com>
2016-01-12 20:49:59 -0800

    Add method to play video

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -1,7 +1,13 @@
 import media
 
-toy_story = media.Moive("Toy Story", 
+toy_story = media.Movie("Toy Story", 
 						"A Story of a boy and his toys that come to life",
 						"http://www.gstatic.com/tv/thumb/movieposters/17420/p17420_p_v7_ab.jpg",
 						"https://www.youtube.com/watch?v=KYz2wyBy3kc")
 print(toy_story.storyline)
+avatar = media.Movie("Avatar",
+					"A marine on an alien planet",
+					"http://cafmp.com/wp-content/uploads/2012/11/avatar.jpg",
+					"https://www.youtube.com/watch?v=5PSNL1qE6VY")
+print avatar.storyline
+avatar.show_trailer()
