23748902b4ac3ebf57d1c19a54d5cb4e7f36efbd
Chris Archibald <carchi8py@gmail.com>
2016-01-12 20:03:31 -0800

    Adding our first class

---
+++ movies/entertainment_center.py
@@ -0,0 +1,7 @@
+import media
+
+toy_story = media.Moive("Toy Story", 
+						"A Story of a boy and his toys that come to life",
+						"http://www.gstatic.com/tv/thumb/movieposters/17420/p17420_p_v7_ab.jpg",
+						"https://www.youtube.com/watch?v=KYz2wyBy3kc")
+print(toy_story.storyline)
